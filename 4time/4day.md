4b8663cdde9b71bdafdcbee67964a1f70f5c2d35
sweeywu <3127484862@qq.com>
2025-04-01 22:37:41 +0800

    time.cc

---
+++ 4time/4day.md
@@ -0,0 +1,20 @@
+## 1
+- bool recurring-这个参数表示定时器是否是周期性的：
+- true：表示定时器会重复触发（即每隔 ms 毫秒触发一次）。
+- false：表示定时器仅触发一次。
+## 2
+- std::set< std::shared_ptr< Timer >, Timer::Comparator> m_timers---------最小堆
+- m_timers 是一个 std::set 容器，std::set< T, Comparator > 是一个有序集合，它的元素类型为 T，并且使用 Comparator 进行排序，这里 T = std::shared_ptr<Timer>，意味着集合中的元素是**Timer 的智能指针**（std::shared_ptr< Timer >），Comparator = Timer::Comparator，这个是 set 用来比较元素顺序的规则，决定哪些 Timer 在前，哪些在后
+- Comparator这个结构体里面的函数名字必须是operator()，并且返回值是bool类型
+
+## 3
+- bool operator()(const std::shared_ptr<Timer>& a,const std::shared_ptr<Timer>& b)const;这李最后一个const的作用是什么?
+- 末尾的 const 是用来保证这个比较函数不会修改 Comparator 结构体的成员变量
+
+## 4
+- m_next = now + std::chrono::milliseconds(m_ms);为什么需要变化std::chrono::milliseconds(m_ms)
+- m_ms 是 uint64_t（整数），std::chrono::milliseconds(m_ms) 会把它转换成 std::chrono::milliseconds 类型，使得 now + ... 计算时，单位是毫秒
+
+## 5
+- std::unique_lock<std::shared_mutex> write_lock(m_manager->m_mutex);
+- 多个线程可以同时读取，但只有一个线程能修改，避免数据竞争
---
+++ 4time/timer.h
@@ -0,0 +1,54 @@
+#include<cstdint>
+#include<functional>
+#include<vector>
+#include<memory>
+#include<chrono>
+#include<set>
+#include<shared_mutex>
+class Timer:public std::enable_shared_from_this<Timer>{
+    friend class TimeManager;
+    private:
+    Timer(uint64_t ms,std::function<void()>cb,bool recurring,TimeManager* manager);
+    uint64_t m_ms;
+    std::function<void()>m_cb;
+    TimeManager* m_manager;
+    //是否循环
+    bool m_recurring;
+    //绝对超时时间
+    std::chrono::time_point<std::chrono::steady_clock> m_next;
+    
+    public:
+    //从时间堆删除Timer
+    bool cancel();
+    //刷新timer
+    bool refresh();
+    //重置timer的超时时间
+    bool reset(uint64_t ms,bool from_now);
+    //比较绝对超时时间函数
+    struct Comparator{
+        bool operator()(const std::shared_ptr<Timer>& a,const std::shared_ptr<Timer>& b)const;
+    };
+};
+class TimeManager{
+   friend class Timer;
+   public:
+   TimeManager();
+   ~TimeManager();
+   std::shared_ptr<Timer> addTimer(uint64_t ms,std::function<void()>cb,bool curring);
+   std::shared_ptr<Timer> addConditionTimer(uint64_t ms,std::function<void()> cb,bool curring,std::weak_ptr<void> weakcond);
+   //堆中是否还有timer
+   bool hasTimer();
+   //取出所有时间的回调函数
+   void listExpiredCb(std::vector<std::function<void()>>cb);
+   //当有一个最早的timer加入堆中，调用它
+   void onTimeInsertedAtFront(){};
+   protected:
+   void addTimer(std::shared_ptr<Timer> timer);
+   private:
+   //时间堆
+   std::set<std::shared_ptr<Timer>,Timer::Comparator> m_timers;
+   //锁
+   std::shared_mutex m_mutex;//读写锁
+   bool m_tickle=false;
+   std::chrono::time_point<std::chrono::steady_clock> m_previousTime;
+};
